41749a0f62aa66c9d07f3a3911137197d86bbd29
George Small <you@example.com>
2021-11-25 08:56:19 +0000

    add checkbox functionality

--- dist/app.js
+++ dist/app.js
@@ -19,10 +19,23 @@ class Task {
     // Bind To Function
     this.checkHandler = this.checkHandler.bind(this);
     // Event Listener
-    this.checkBtn.addEventListener("click", this.checkHandler)
+    this.checkBtn.addEventListener("click", this.checkHandler);
+    this.checked = false;
   }
 
   checkHandler() {
+    if (!this.checked) {
+      this.toggleBackgroundHandler();
+      this.checked = true;
+      myApp.displayTasks(-1)
+    } else if (this.checked) {
+      this.toggleBackgroundHandler();
+      this.checked = false;
+      myApp.displayTasks(1)
+    }
+  }
+
+  toggleBackgroundHandler() {
     this.checkBtn.classList.toggle("blue-background");
   }
 }
@@ -35,19 +48,17 @@ class UI {
     this.clearBtn = getElement(".clear-btn");
     this.itemNumber = getElement(".number-left");
     this.taskList = [];
-    // Bindes
-    this.displayTasks = this.displayTasks.bind(this);
+    this.counter = 0;
+
     // Event Listeners
     this.input.addEventListener("keypress", (e) => {
       if (e.charCode === 13 && this.input.value.length > 0) {
         this.addTask(this.input.value);
         this.input.value = "";
-        this.displayTasks();
+        this.displayTasks(1);
       }
     });
-
   }
-
 
   addTask(v) {
     const task = document.createElement("div");
@@ -58,14 +69,16 @@ class UI {
   }
 
 
-  displayTasks() {
+  displayTasks(c) {
+    this.counter += c;
+    this.itemNumber.textContent = this.counter;
     if (this.taskList.length > 0) {
       this.taskList.forEach((task) => {
         this.tasks.appendChild(task.value);
       });
     }
-  }
-
+  };
 }
+
 
 const myApp = new UI();
